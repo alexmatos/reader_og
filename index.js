a4ba472f139f824dee72885578fbdfd4ea951eab
Alex Matos da Silva <alex.matos@ufms.br>
2019-04-25 17:17:21 -0400

    Funções para ler open graph

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const readlineSync = require('readline-sync');
-const ogs = require('open-graph-scraper');
-
-const url = readlineSync.question('Entre com uma URL: ');
-
-console.log('Processando...');
+const axios = require('axios');
 
 
-let options = {'url': url};
-ogs(options, function (error, results) {
-  console.log('error:', error); 
-  console.log('results:', results);
-});
+const readUrl = function () {
+  const pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gi
+  const regex = new RegExp(pattern)
+  const url = readlineSync.question('Entre com uma URL: ');
+
+  return url.match(regex) ? url : null
+}
+
+const downloadPage = (url, callback) => {
+  axios.get(url, { responseType: 'text' })
+    .then(result => callback(null, result))
+    .catch(err => callback(err, null))
+}
+
+const scrapingOG = (err, page) => {
+  if (err) console.log(err)
+
+  console.log(page)
+}
+
+const main = function () {
+
+  const url = readUrl()
+  if (!url) {
+    console.log('URL inválida')
+    return
+  }
+
+  downloadPage(url, scrapingOG)
+
+}
+
+main()
